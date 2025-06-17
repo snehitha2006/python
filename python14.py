a15f31f557690fb83ea00ae039fa4f03dfed32ae
snehitha <peddisettisnehitha06@gmail.com>
2025-06-17 22:12:19 +0530

    Update python14.py

--- python14.py
+++ python14.py
@@ -2,7 +2,7 @@
 a=5
 b=3
 print("values before swapping are a=",a,"\t","b=",b)
-a=a+b
-a=a-b
-b=a-b
+a=a+b      | a=a*b    | a=a^b
+a=a-b      | a=a//b   | b=a^b
+b=a-b      |b=a//b    | a=a^b
 print("values after swapping area=",a,"\t","b=",b)
