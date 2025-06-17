d3b2987fa27e3f222aef9110d6248cd83b0249ee
snehitha <peddisettisnehitha06@gmail.com>
2025-06-17 22:19:20 +0530

    Create python16.py

---
+++ python16.py
@@ -0,0 +1,8 @@
+a=5,b=3
+printf("addition=",(a+b))
+print("subtraction=",(a-b))
+print("multiplication=",(a*b))
+print("division=",(a/b))
+print("modulus=",(a%b))
+print("floor division=",(a//b))
+print("exponentiation=",(a**b))
