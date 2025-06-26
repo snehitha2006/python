f336a6b350c6b5a16d6bc29097ad5654ca24921c
snehitha <peddisettisnehitha06@gmail.com>
2025-06-26 21:06:15 +0530

    Update python22.py

--- python22.py
+++ python22.py
@@ -1,6 +1,6 @@
 #swap values by accepting user input from the keyboard
 a=input("enter a first value")
 b=input("enter the second value")
-print("before swapping are a=,"a","\t","b="'b)
+print("before swapping are a=",a,"\t","b=",b)
 a,b=b,a
 print("values after swapping are a=",a,"\t","b=",b)
