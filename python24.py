ff8c44c254bc65b02c7ecacd0366ae422ed4f331
snehitha <peddisettisnehitha06@gmail.com>
2025-06-26 21:00:28 +0530

    Create python24.py

---
+++ python24.py
@@ -0,0 +1,5 @@
+#program to print area of rectangle
+length=float(input("enter length of the rectangle"))
+width=float(input("enter the width of rectangle"))
+area=length*width
+print("area of rectangle=",area)
