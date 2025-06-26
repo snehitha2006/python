0a5f253df78cdbb2222334ed30315f383a5e8582
snehitha <peddisettisnehitha06@gmail.com>
2025-06-26 21:10:47 +0530

    Create python25.py

---
+++ python25.py
@@ -0,0 +1,5 @@
+#program to print area of traingle
+base=float(input("enter the base of traingle"))
+height=float(input("enter the height of traingle"))
+area=0.5*base*height
+print("area of traingle=",area)
